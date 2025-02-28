630d40665c079f23858d9de6503215a037c8f4c3
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-02-28 12:04:44 +0100

    add flags

--- src/assets/components/MoviesCard.jsx
+++ src/assets/components/MoviesCard.jsx
@@ -1,10 +1,19 @@
 const MoviesCard = ({ movie }) => {
+
+    // Costruiamo il percorso della bandiera
+    const flagSrc = `/flags/${movie.language}.jpg`;
+
     return (
-        <div>
+        <div className="movie-card">
             <img src={movie.poster} alt={movie.title} />
             <h3>{movie.title}</h3>
             <p><strong>Titolo Originale:</strong> {movie.originalTitle}</p>
-            <p><strong>Lingua:</strong> {movie.language.toUpperCase()}</p>
+            <p><strong>Lingua:</strong> <img
+                className="flag-icon"
+                src={flagSrc}
+                onError={(e) => { e.target.src = "/flags/default.jpg"; }}
+                alt={movie.language}
+            /></p>
             <p><strong>Voto:</strong> ⭐ {movie.rating}</p>
         </div>
     );
