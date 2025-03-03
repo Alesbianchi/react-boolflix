a582cc00e02c38231631f73c44d787b44bce87a3
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-03-03 04:40:56 +0100

    add fontawensome and stars rating

--- src/assets/components/MoviesCard.jsx
+++ src/assets/components/MoviesCard.jsx
@@ -1,4 +1,13 @@
+//importo fontawensome 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
+
+
 const MoviesCard = ({ movie }) => {
+
+    // Converto il voto da 1-10 a 1-5 e arrotondo per eccesso
+    const rating = Math.ceil(movie.rating / 2);
 
     // Costruiamo il percorso della bandiera
     const flagSrc = `/flags/${movie.language}.jpg`;
@@ -16,8 +25,18 @@ const MoviesCard = ({ movie }) => {
                     onError={(e) => { e.target.src = "/flags/default.jpg"; }}
                     alt={movie.language}
                 /></p>
-                <p><strong>Voto:</strong> ⭐ {movie.rating}</p>
+                {/* <p><strong>Voto:</strong> ⭐ {movie.rating}</p> */}
+                <div className="movie-rating">
+                    {[...Array(5)].map((_, i) => (
+                        <FontAwesomeIcon
+                            key={i}
+                            icon={i < rating ? fullStar : emptyStar}
+                            className="star-icon"
+                        />
+                    ))}
+                </div>
             </div>
+
         </div>
     );
 };
